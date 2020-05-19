f2ae6edf66ea66042c74221e6ebbf94d42fafeba
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-20 00:38:24 +0300

    veritabanı oluşturuldu.

--- CourseProject/Startup.cs
+++ CourseProject/Startup.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CourseProject.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 
@@ -13,11 +16,18 @@ namespace CourseProject
 {
     public class Startup
     {
+        public IConfiguration Configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }//datacontext tanımlama
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DataConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
