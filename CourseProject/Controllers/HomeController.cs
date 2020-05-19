f51f8e6daebfee7d627bc3d35f3cfbce887413ef
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-19 16:40:32 +0300

    Projeye başlandı.

---
+++ CourseProject/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            CourseProject.Models.Request model = new Models.Request();
+
+            model.Name = "Kubilay";
+            model.Email = "kubilayyazi@hotmail.com";
+            model.Phone = "0537345894";
+            model.Message = "Kursa Başlandı";
+                
+
+            return View(model);
+        }
+    }
+}
