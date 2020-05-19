f1eba11ce29efb3884ac520055636d29751999c3
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-19 23:31:31 +0300

    clientside bower ve npm ile paketlerinin eklenmesi tamamlandı.

--- CourseProject/Startup.cs
+++ CourseProject/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace CourseProject
 {
@@ -34,6 +36,10 @@ namespace CourseProject
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),RequestPath=new PathString("/vendor")
+            });
             app.UseMvcWithDefaultRoute();// controller/action/?id
            
         }
