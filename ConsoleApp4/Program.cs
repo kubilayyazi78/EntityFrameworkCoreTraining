b3afef53d12fb1a78c791489d6b6058f3efb88cd
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-31 16:27:15 +0300

    Stored Procedure ile uygulama katmanında çalışıldı.

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,6 +4,7 @@ using ConsoleApp4.Models.EfCore;
 using ConsoleApp4.Models.FluentApi;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Data.SqlClient;
 using System.Linq;
 
 namespace ConsoleApp4
@@ -35,24 +36,57 @@ namespace ConsoleApp4
 
 
 
-            using (MovieContext db=new MovieContext())
-            {
-                string name = "Movie1";
-                var movies = db.Movie.FromSql($"select * from dbo.Movie where Name={name}")
-                    .Select(i => new {
-                        i.Name
-                    })
-                    .OrderBy(i=>i.Name)
-                    .ToList();
+            //using (MovieContext db=new MovieContext())
+            //{
+            //    string name = "Movie1";
+            //    var movies = db.Movie.FromSql($"select * from dbo.Movie where Name={name}")
+            //        .Select(i => new {
+            //            i.Name
+            //        })
+            //        .OrderBy(i=>i.Name)
+            //        .ToList();
 
-                foreach (var item in movies)
-                {
-                    Console.WriteLine(item.Name);
-                }
+            //    foreach (var item in movies)
+            //    {
+            //        Console.WriteLine(item.Name);
+            //    }
+            //}
+
+            //using (MovieContext db = new MovieContext())
+            //{
+            //    string name = "Dark";
+            //    var movies = db.Movie.FromSql($"GetMoviesByName {name}").ToList();
+
+            //    foreach (var movie in movies)
+            //    {
+            //        Console.WriteLine(movie.Name);
+            //    }
+            //}
+
+
+
+            using (MovieContext db = new MovieContext())
+            {
+                string name = "Dark";
+                //var movies = db.Database.ExecuteSqlCommand("insert into Movie(name) values ('deneme')");
+
+               // var p0 = new SqlParameter("@Name", "Movie");
+
+
+
+                var movies = db.Database.ExecuteSqlCommand("CreateMovie @name,@name,@name",new[] {"aa","bb","cc"});
+
+                //foreach (var movie in movies)
+                //{
+                //    Console.WriteLine(movie.Name);
+                //}
+
+
+                Console.WriteLine(movies);
             }
 
 
-                Console.ReadKey();
+            Console.ReadKey();
         }
     }
 }
