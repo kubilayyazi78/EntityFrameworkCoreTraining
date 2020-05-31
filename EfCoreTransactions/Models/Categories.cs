86d160f02811f1ab9a134c73aab614fd0bc6166f
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-31 17:41:02 +0300

    Transaction oluşturuldu.

---
+++ EfCoreTransactions/Models/Categories.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EfCoreTransactions.Models
+{
+    public partial class Categories
+    {
+        public Categories()
+        {
+            Products = new HashSet<Products>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Products> Products { get; set; }
+    }
+}
---
+++ EfCoreTransactions/Program.cs
@@ -0,0 +1,51 @@
+﻿using EfCoreTransactions.Models;
+using System;
+using System.Linq;
+
+namespace EfCoreTransactions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NorthwindContext db = new NorthwindContext())
+            {
+
+                using (var transaction = db.Database.BeginTransaction())
+                {
+
+
+
+                    try
+                    {
+                        var products = db.Products.Take(5).ToList();
+
+                        foreach (var product in products)
+                        {
+                            Console.WriteLine("updating product : {0}", product.ProductId);
+
+                            product.UnitsInStock += 100;
+
+                            if (product == products[4])
+                            {
+                                product.UnitsInStock += 32750;
+                            }
+                        }
+
+                        db.SaveChanges();
+
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                        Console.WriteLine("error");
+                    }
+
+
+
+                }
+            }
+        }
+    }
+}
